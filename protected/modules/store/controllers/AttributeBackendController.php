71fee9a1256022e3a9c4c412fe615118aa41bb71
yuri_korenyuk <yurakorenyk@gmail.com>
2016-02-25 16:01:11 +0300

    init commit

---
+++ protected/modules/store/controllers/AttributeBackendController.php
@@ -0,0 +1,226 @@
+<?php
+
+/**
+ * Class AttributeBackendController
+ */
+class AttributeBackendController extends yupe\components\controllers\BackController
+{
+    /**
+     * @return array
+     */
+    public function accessRules()
+    {
+        return [
+            ['allow', 'roles' => ['admin'],],
+            ['allow', 'actions' => ['index'], 'roles' => ['Store.AttributeBackend.Index'],],
+            ['allow', 'actions' => ['create', 'groupCreate'], 'roles' => ['Store.AttributeBackend.Create'],],
+            [
+                'allow',
+                'actions' => ['update', 'sortable', 'inlineEditGroup', 'groupCreate'],
+                'roles' => ['Store.AttributeBackend.Update'],
+            ],
+            ['allow', 'actions' => ['delete', 'multiaction'], 'roles' => ['Store.AttributeBackend.Delete'],],
+            ['deny',],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function actions()
+    {
+        return [
+            'inlineEditGroup' => [
+                'class' => 'yupe\components\actions\YInLineEditAction',
+                'model' => 'AttributeGroup',
+                'validAttributes' => ['name'],
+            ],
+            'sortable' => [
+                'class' => 'yupe\components\actions\SortAction',
+                'model' => 'AttributeGroup',
+            ],
+        ];
+    }
+
+
+    /**
+     * Создает новую модель атрибута.
+     * Если создание прошло успешно - перенаправляет на просмотр.
+     *
+     * @return void
+     */
+    public function actionCreate()
+    {
+        $model = new Attribute();
+
+        if (($data = Yii::app()->getRequest()->getPost('Attribute')) !== null) {
+
+            $model->setAttributes($data);
+
+            if ($model->save()) {
+                Yii::app()->user->setFlash(
+                    yupe\widgets\YFlashMessages::SUCCESS_MESSAGE,
+                    Yii::t('StoreModule.store', 'Attribute created')
+                );
+
+                $this->redirect(
+                    (array)Yii::app()->getRequest()->getPost(
+                        'submit-type',
+                        ['create']
+                    )
+                );
+            }
+        }
+
+        $this->render('create', ['model' => $model]);
+    }
+
+
+    /**
+     * @param $id
+     * @throws CHttpException
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->loadModel($id);
+
+        if (($data = Yii::app()->getRequest()->getPost('Attribute')) !== null) {
+            $currentType = $model->type;
+            $model->setAttributes(Yii::app()->getRequest()->getPost('Attribute'));
+
+            if ($model->save() && $model->changeType($currentType, $model->type)) {
+
+                Yii::app()->user->setFlash(
+                    yupe\widgets\YFlashMessages::SUCCESS_MESSAGE,
+                    Yii::t('StoreModule.store', 'Attribute updated')
+                );
+
+                $this->redirect(
+                    (array)Yii::app()->getRequest()->getPost(
+                        'submit-type',
+                        [
+                            'update',
+                            'id' => $model->id,
+                        ]
+                    )
+                );
+            }
+        }
+
+        $this->render(
+            'update',
+            [
+                'model' => $model,
+            ]
+        );
+    }
+
+    /**
+     * @param $id
+     * @throws CHttpException
+     */
+    public function actionDelete($id)
+    {
+        if (Yii::app()->getRequest()->getIsPostRequest()) {
+
+            $transaction = Yii::app()->getDb()->beginTransaction();
+
+            try {
+                // поддерживаем удаление только из POST-запроса
+                $this->loadModel($id)->delete();
+
+                $transaction->commit();
+
+                if (!isset($_GET['ajax'])) {
+                    $this->redirect(
+                        (array)Yii::app()->getRequest()->getPost('returnUrl', 'index')
+                    );
+                }
+            } catch (Exception $e) {
+                $transaction->rollback();
+                Yii::log($e->__toString(), CLogger::LEVEL_ERROR);
+            }
+
+        } else {
+            throw new CHttpException(
+                400,
+                Yii::t('StoreModule.store', 'Bad request. Please don\'t use similar requests anymore')
+            );
+        }
+    }
+
+
+    /**
+     *
+     */
+    public function actionIndex()
+    {
+        $model = new Attribute('search');
+        $model->unsetAttributes();
+
+        $attributeGroup = new AttributeGroup('search');
+        $attributeGroup->unsetAttributes();
+
+        if (isset($_GET['Attribute'])) {
+            $model->setAttributes($_GET['Attribute']);
+        }
+
+        $this->render('index', [
+            'model' => $model,
+            'attributeGroup' => $attributeGroup
+        ]);
+    }
+
+    /**
+     * Возвращает модель по указанному идентификатору
+     * Если модель не будет найдена - возникнет HTTP-исключение.
+     *
+     * @param integer $id - идентификатор нужной модели
+     *
+     * @return Attribute $model
+     *
+     * @throws CHttpException
+     */
+    public function loadModel($id)
+    {
+        $model = Attribute::model()->findByPk($id);
+        if ($model === null) {
+            throw new CHttpException(404, Yii::t('StoreModule.store', 'Page not found!'));
+        }
+
+        return $model;
+    }
+
+    /**
+     * Производит AJAX-валидацию
+     *
+     * @param CModel $model - модель, которую необходимо валидировать
+     *
+     * @return void
+     */
+    protected function performAjaxValidation(Attribute $model)
+    {
+        if (Yii::app()->getRequest()->getIsAjaxRequest() && Yii::app()->getRequest()->getPost(
+                'ajax'
+            ) === 'attribute-form'
+        ) {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
+    /**
+     *
+     */
+    public function actionGroupCreate()
+    {
+        if (Yii::app()->getRequest()->getIsPostRequest()) {
+            $group = new AttributeGroup();
+            $group->name = Yii::app()->getRequest()->getParam('name');
+            if ($group->save()) {
+                Yii::app()->ajax->success();
+            }
+            Yii::app()->ajax->failure($group->getErrors());
+        }
+    }
+}
